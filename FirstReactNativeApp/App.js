393af1fc1c6e45b6182e45690d6e5d85fb9e616e
52200782 <52200782+SupunNanayakkara@users.noreply.github.com>
2021-07-26 18:30:34 +0530

    Functional Component Example 1

--- FirstReactNativeApp/App.js
+++ FirstReactNativeApp/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Text, View } from "react-native";
 
 //Class Component 
-class App extends Component{
+/*class App extends Component{
   render(){
     return(
       <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
@@ -10,6 +10,15 @@ class App extends Component{
       </View>
     );
   }
+}*/
+
+//Functional Component
+const App = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Hello World!</Text>
+    </View>
+  );
 }
 
 export default App;
